d2d7850c1fb660e0947a52819f1074e2cf06c915
Marcos Nicolau <marcos.nicolau.external@tkelevator.com>
2022-04-01 10:51:29 -0300

    criação inicial do projeto

---
+++ PokedexEgsys/app/src/main/java/com/marcosk/pokedexegsys/ui/PokemonListActivity.kt
@@ -0,0 +1,14 @@
+package com.marcosk.pokedexegsys.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PokemonListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
